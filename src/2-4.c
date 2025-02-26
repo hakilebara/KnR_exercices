f98af73f44e54dc91a7dd96836e1fb9e4aa3bfe9
Fred ST <frederic.soumare@gmail.com>
2025-02-26 10:48:58 +0100

    Refactor exercice 2-4
    Refactor the exercice code to use end of string detection ('\0') instead
    of a dedicated variable. 'Squeezes' one variable away and make the code
    more elegant imo

--- src/2-4.c
+++ src/2-4.c
@@ -5,17 +5,12 @@
 #include <stdio.h>
 
 void squeeze(char s1[], char s2[]) {
-  int match, i, j, k;
+  int i, j, k;
   for (i = j =  0; s1[i] != '\0'; ++i) {
-    match = 0;
-    for (k = 0; s2[k] != '\0'; ++k) {
-      if (s2[k] == s1[i]) { // match found
-        match = 1;
-      }
-    }
-    if (!match) {
+    for (k = 0; s2[k] != '\0' && s2[k] != s1[i]; ++k)
+      ;
+    if (s2[k] == '\0') // no match was found in s2
       s1[j++] = s1[i];
-    }
   }
   s1[j] = '\0';
 }
@@ -23,12 +18,12 @@ void squeeze(char s1[], char s2[]) {
 int main(void) {
   char s[] = "Strawberry";
   char t[] = "ra";
-  printf("Original: %21s\n", s);
-  printf("Characters squeezed: ");
+  printf("input:\t%s\n", s);
+  printf("chars:\t");
   for (int i = 0; t[i] != '\0'; ++i) {
     printf("%c ", t[i]);
   }
   printf("\n");
   squeeze(s, t);
-  printf("Squeezed: %17s\n", s);
+  printf("output:\t%s\n", s);
 }
