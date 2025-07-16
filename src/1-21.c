3b507ff12b118f720c1d3ae78d2538825156bf44
Fred ST <frederic.soumare@gmail.com>
2025-07-16 16:59:04 +0200

    Refactor Exercice 1-21
    Make the code less verbose
    Use a for loop to initialise and increment the cursor position

--- src/1-21.c
+++ src/1-21.c
@@ -6,44 +6,30 @@
  *
  * This is a naive implementation of entab that only handles blanks spaces ' '
  */
-
 #include <stdio.h>
 
 #define TAB_STOP  4
-                
+
 int main() {
-  int c, pos, spaces;
-  pos = spaces = 0;
-  while ((c = getchar()) != EOF) {
-
-    // At every tabstop, I print the spaces I have counted
-    if (pos % TAB_STOP == 0) {
-      if (spaces == TAB_STOP) {
-        putchar('\t');
-        spaces = 0; 
-      }
-      else {
-        for (; spaces > 0; --spaces) {
-          putchar(' ');
-        }
-      }
-    } 
-
+  int c, nb, pos;
+  nb = 0; // counter of blank spaces
+  for (pos = 1; (c = getchar()) != EOF; ++pos) {
     if (c == ' ') {
-      ++spaces;
-      ++pos;
-    } else if (c == '\n') { // reset counters on new line
-      putchar(c);
-      pos = 0;
-      spaces = 0;
-    } else {
-      // If I encounter a non blank char, I print the spaces I have counted
-      for (; spaces > 0; --spaces) {
-        putchar(' ');
+      // use default tab settings if there are no arguments
+      if (pos % TAB_STOP == 0) {
+        putchar('\t');
+        nb = 0;
       }
-      spaces = 0;
+      else
+        ++nb;
+    }
+    else {
+      for (; nb > 0; --nb)
+        putchar(' ');
+      if ( c == '\n' )
+        pos = 0;
       putchar(c);
-      ++pos;
+      nb = 0;
     }
   }
 }
